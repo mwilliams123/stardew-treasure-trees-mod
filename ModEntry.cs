f576736cfdea631838c62d1d9230899563263e27
Megan Williams <wilm140@ucla.edu>
2023-03-29 02:45:03 -0600

    Add tests

--- ModEntry.cs
+++ ModEntry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using MoreTreasureRoomTrees.Patches;
 using HarmonyLib;
@@ -12,6 +13,7 @@ using StardewValley;
 namespace MoreTreasureRoomTrees
 {
     /// <summary>The mod entry point.</summary>
+    
     internal sealed class ModEntry : Mod
     {
         public List<int> saplings;
@@ -25,6 +27,7 @@ namespace MoreTreasureRoomTrees
         {
             Instance = this;
             this.Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            //this.Helper.Events.GameLoop.DayStarted += OnDayStarted; // For testing purposes only.
         }
 
         /*********
@@ -39,6 +42,28 @@ namespace MoreTreasureRoomTrees
             this.saplings = fruitTrees.Keys.ToList();
             this.Monitor.Log($"Found {this.saplings.Count} types of trees.", LogLevel.Debug);
             ApplyHarmonyPatches();
+        }
+
+        /// <summary>Raised after starting a new day.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        /// <remarks>This is only used to test mod & verify rewards have desired distribution.</remarks>
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            Dictionary<string, int> dist = new Dictionary<string, int>();
+            int n = 100000;
+            for (int i = 0; i < n; i++)
+            {
+                Item treasure = StardewValley.Locations.MineShaft.getTreasureRoomItem();
+                int total;
+                dist[treasure.Name] = dist.TryGetValue(treasure.Name, out total) ? total + 1 : 1;
+            }
+            string s = "{\n";
+            foreach (var d in dist)
+            {
+                s += $"\"{d.Key}\" : {d.Value},\n";
+            }
+            this.Monitor.Log(s + '}', LogLevel.Debug);
         }
 
         /// <summary>Applies harmony patches for patching source code.</summary>
